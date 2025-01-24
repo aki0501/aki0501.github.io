3ee91e02c5cc3dfe322f0bd40ed95e4fec103153
Aki Hasegawa-Johnson <aki0501@g.ucla.edu>
2025-01-24 00:42:53 -0800

    Fix viewport sizing issue

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ export default function Highlight1() {
         <div class="aboutme">
             <section class="section">
             <h3>Education</h3>
-            <p>Currently a student at the University of California, Los Angeles studying Computer Science and minoring in Music Industry and Film, Television, & Digital Media.</p>
+            <p>Currently a student at the University of California, Los Angeles studying Computer Science and double minoring in Music Industry and Film, Television, & Digital Media.</p>
             <p>A bunch of text. A bunch of text.A bunch of text.A bunch of text.A bunch of text.A bunch of text.A bunch of text.A bunch of text.A bunch of text.A bunch of text.A bunch of text.A bunch of text.</p>
             </section>
 
